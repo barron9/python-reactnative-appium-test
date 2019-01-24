a9b6c1fa1d139008bbbe19df6f27f3c2bd8c7b21
berkin.tatlisu <berkin.tatlisu@NB-0201>
2019-01-24 18:29:41 +0300

    test

---
+++ readme.txt
@@ -0,0 +1,1 @@
+https://medium.com/@liheyse/how-to-do-end-to-end-e2e-testing-for-react-native-android-on-real-devices-using-python-appium-800cd6c62541
---
+++ test.py
@@ -0,0 +1,38 @@
+import os
+import time
+from appium import webdriver
+
+driver = webdriver.Remote(
+    command_executor='http://127.0.0.1:4723/wd/hub',
+    desired_capabilities={
+        'app': 
+os.path.expanduser('C:/Users/berkin.tatlisu/Desktop/shop/client/android/app/build/outputs/apk/debug/app-debug.apk'),
+        'platformName': 'Android',
+        'deviceName': 'Nexus_5X_API_21',
+    })
+
+# wait for app to load
+time.sleep(10)
+
+#asd = driver.back()
+
+# find the link with the text "Click here" and click on it
+#link = 
+#driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View[1]')
+
+#link = driver.find_element_by_accessibility_id('test1')
+#link =
+link = driver.find_element_by_accessibility_id("test1")
+link.click()
+link.click()
+#link.click()
+
+# wait for the next screen to load
+##time.sleep(10)
+
+# make sure the correct "Success" result is on the page
+#driver.find_element_by_xpath('//*[@text="Success"]')
+
+# important; you will not be able to launch again if this does not 
+#happen
+driver.quit()
